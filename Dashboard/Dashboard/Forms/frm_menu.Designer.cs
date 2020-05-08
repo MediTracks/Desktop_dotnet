9acedb4895e8e086d2a22e29d398ff997a53c0e1
Lens Mutombo <lensmutombo@gmail.com>
2020-05-08 23:50:31 +0200

    Update frm_menu.Designer.cs

--- Dashboard/Dashboard/Forms/frm_menu.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_menu.Designer.cs
@@ -32,9 +32,8 @@
             // 
             // frm_menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 734);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1284, 561);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_menu";
