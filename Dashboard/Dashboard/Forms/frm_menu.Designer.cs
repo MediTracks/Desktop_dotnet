bb735497df23829003f4ef72d1072fc478877f45
Lens Mutombo <lensmutombo@gmail.com>
2020-05-08 22:36:41 +0200

    Update frm_menu.Designer.cs

--- Dashboard/Dashboard/Forms/frm_menu.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_menu.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1284, 561);
+            this.Name = "frm_menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frm_menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
