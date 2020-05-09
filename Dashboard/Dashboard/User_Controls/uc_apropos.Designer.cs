9ca93dc8af604194a53bbf155f4acfd1a4586de2
Lens Mutombo <lensmutombo@gmail.com>
2020-05-09 23:02:26 +0200

    Update uc_apropos.Designer.cs

--- Dashboard/Dashboard/User_Controls/uc_apropos.Designer.cs
+++ Dashboard/Dashboard/User_Controls/uc_apropos.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // uc_apropos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "uc_apropos";
+            this.Size = new System.Drawing.Size(1122, 566);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
