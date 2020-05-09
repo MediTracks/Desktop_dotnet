4c390f73ed8235c79d6c78b355ae7c39ec894aee
Lens Mutombo <lensmutombo@gmail.com>
2020-05-09 23:03:30 +0200

    Update uc_accueil.Designer.cs

--- Dashboard/Dashboard/User_Controls/uc_accueil.Designer.cs
+++ Dashboard/Dashboard/User_Controls/uc_accueil.Designer.cs
@@ -32,13 +32,13 @@
             // 
             // uc_accueil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Ravie", 8.25F);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "uc_accueil";
-            this.Size = new System.Drawing.Size(1217, 565);
+            this.Size = new System.Drawing.Size(1369, 565);
             this.ResumeLayout(false);
 
         }
