2f640e68fae03be407dbc272a908d6b8b9bda6ef
Lens Mutombo <lensmutombo@gmail.com>
2020-05-10 22:29:47 +0200

    Update frm_menu.Designer.cs
    Panel Sized

--- Dashboard/Dashboard/Forms/frm_menu.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_menu.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(222, 561);
             this.panel1.TabIndex = 0;
             // 
             // frm_menu
