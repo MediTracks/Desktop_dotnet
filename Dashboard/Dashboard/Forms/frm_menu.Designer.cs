15db935ecc3bb62df027fb56decffc8a1b8c9e74
Lens Mutombo <lensmutombo@gmail.com>
2020-05-11 16:55:42 +0200

    Update frm_menu.Designer.cs
    Adjusted for fit

--- Dashboard/Dashboard/Forms/frm_menu.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_menu.Designer.cs
@@ -50,7 +50,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(66, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
