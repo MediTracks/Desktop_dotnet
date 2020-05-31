9ce77bfd9f5b0361a18216200de350d1e6534818
Lens Mutombo <lensmutombo@gmail.com>
2020-05-31 22:24:53 +0200

    Update frm_splash.Designer.cs

--- Dashboard/Dashboard/Forms/frm_splash.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_splash.Designer.cs
@@ -53,7 +53,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_splash";
-            this.Text = "frm_splash";
+            this.Opacity = 0.96D;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
