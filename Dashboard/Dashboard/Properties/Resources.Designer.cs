67c00fb0175657e2422a4474c1717e98c927cd64
Lens Mutombo <lensmutombo@gmail.com>
2020-05-31 22:23:11 +0200

    Update Resources.Designer.cs

--- Dashboard/Dashboard/Properties/Resources.Designer.cs
+++ Dashboard/Dashboard/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Dashboard.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Meditracks {
+            get {
+                object obj = ResourceManager.GetObject("Meditracks", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Meditracks___short {
             get {
                 object obj = ResourceManager.GetObject("Meditracks - short", resourceCulture);
