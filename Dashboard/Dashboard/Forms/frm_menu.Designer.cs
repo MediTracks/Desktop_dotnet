8d56233e8e72863e3f282164ad014cffad44bc63
Lens Mutombo <lensmutombo@gmail.com>
2020-06-03 22:50:34 +0200

    Update frm_menu.Designer.cs

--- Dashboard/Dashboard/Forms/frm_menu.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_menu.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_menu));
             this.pnl_main_menu = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pnl_main_menu.SuspendLayout();
@@ -62,6 +63,7 @@
             this.Controls.Add(this.pnl_main_menu);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_menu";
             this.Opacity = 0.95D;
