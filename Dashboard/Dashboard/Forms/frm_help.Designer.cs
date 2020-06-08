019a203f4e3dff3ee1c2b527ed0738bb3079758b
Lens Mutombo <lensmutombo@gmail.com>
2020-06-08 23:17:50 +0200

    Update frm_help.Designer.cs

--- Dashboard/Dashboard/Forms/frm_help.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_help.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1060, 530);
+            this.Name = "frm_help";
             this.Text = "frm_help";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
