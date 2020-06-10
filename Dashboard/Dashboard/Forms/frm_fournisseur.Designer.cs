375218032d806941ec61e9762bd5c61c812488d9
Lens Mutombo <lensmutombo@gmail.com>
2020-06-10 23:06:24 +0200

    Update frm_fournisseur.Designer.cs

--- Dashboard/Dashboard/Forms/frm_fournisseur.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_fournisseur.Designer.cs
@@ -32,10 +32,12 @@
             // 
             // frm_fournisseur
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 529);
+            this.ClientSize = new System.Drawing.Size(1386, 692);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_fournisseur";
             this.Text = "frm_fournisseur";
             this.ResumeLayout(false);
