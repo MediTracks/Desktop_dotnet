65ef02c5bfa587daba43ac28c20c8f93b3bb803d
Lens Mutombo <lensmutombo@gmail.com>
2020-06-11 23:26:09 +0200

    Update frm_produits.Designer.cs

--- Dashboard/Dashboard/Forms/frm_produits.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_produits.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(96, 105);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(1067, 62);
             this.panel1.TabIndex = 0;
             // 
             // frm_produits
