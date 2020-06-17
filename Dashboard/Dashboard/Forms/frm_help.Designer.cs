6365a12d025a8cb139d612201bf733f3607fcea5
Lens Mutombo <lensmutombo@gmail.com>
2020-06-17 22:56:29 +0200

    Update frm_help.Designer.cs

--- Dashboard/Dashboard/Forms/frm_help.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_help.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(86, 27);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(1360, 60);
             this.panel1.TabIndex = 0;
             // 
             // frm_help
