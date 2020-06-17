f156540f678c686af44733bc69e00eeea8458e1d
Lens Mutombo <lensmutombo@gmail.com>
2020-06-17 22:59:42 +0200

    Update frm_produits.Designer.cs

--- Dashboard/Dashboard/Forms/frm_produits.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_produits.Designer.cs
@@ -36,7 +36,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1067, 62);
+            this.panel1.Size = new System.Drawing.Size(1211, 62);
             this.panel1.TabIndex = 0;
             // 
             // frm_produits
@@ -44,7 +44,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
-            this.ClientSize = new System.Drawing.Size(1067, 580);
+            this.ClientSize = new System.Drawing.Size(1211, 561);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
