11f1c52f5413bc4b7f8a6c08e63a2171935eed1e
Lens Mutombo <lensmutombo@gmail.com>
2020-06-17 22:57:25 +0200

    Update frm_splash.Designer.cs

--- Dashboard/Dashboard/Forms/frm_splash.Designer.cs
+++ Dashboard/Dashboard/Forms/frm_splash.Designer.cs
@@ -35,10 +35,11 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = global::Dashboard.Properties.Resources.Meditracks;
-            this.pictureBox1.Location = new System.Drawing.Point(98, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(473, 246);
+            this.pictureBox1.Size = new System.Drawing.Size(671, 422);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
